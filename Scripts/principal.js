b314a7fa337295a340b104c1c6724e779ad34af8
AlejandraRoc <alejandra_rocha@outlook.es>
2022-04-05 07:40:33 -0500

    corrigiendo errores

--- Scripts/principal.js
+++ Scripts/principal.js
@@ -1,17 +1,47 @@
-import { getData } from "./getData.js";
+import {getData} from "./getData.js";
 import {showData} from "./showData.js"
 
 
 let btnMarvel = document.getElementById("btnCategory1")
-let btnDc     = document.getElementById("btnCategory2")
+let btnDC   = document.getElementById("btnCategory2")
 let listarCard= document.getElementById("listarCard")
 
 btnMarvel.addEventListener('click', async () => {
-  const array = await getData('https://api-4-abril.herokuapp.com/marvel')
+  const array = await getData('http://localhost:4080/marvel')
   showData(array, listarCard)
 })
 
 btnDC.addEventListener('click', async() => {
-  const array = await getData('https://api-4-abril.herokuapp.com/dc')
+  const array = await getData('http://localhost:4080/dc')
   showData(array,listarCard)
 })
+
+const getUrl = async(url)=>{
+  const  resp = await fetch(url)
+  const data = await resp.json(getUrl)
+  console.table(data)
+
+  listarCard.innerHTML = ''
+
+  data.forEach(heroe =>{
+      const {nombre, imagen, historia}=heroe
+
+      listarCard.innerHTML +=`
+      <div class="card" style="width: 18rem;">
+      <img src=${imagen} class="card-img-top" alt="...">
+      <div class="card-body">
+          <h1>${nombre}</h1>
+        <p class="card-text">${historia}</p>
+      </div>
+    </div>
+      `
+
+
+
+  })
+
+
+}
+
+
+
